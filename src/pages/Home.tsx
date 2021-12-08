8741c9bcdf79dc2a667eb536643960eac005a5d3
Amadeusvh <gabrielvilarmartins@gmail.com>
2021-12-08 19:22:27 -0300

    testing the styled components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Title, Button } from "../components/components";
+
+const Home = () => {
+  return (
+    <div>
+      <Title >Welcome to my web page \0/</Title>
+      <Button>Curriculum</Button>
+      <Button>Projects</Button>
+    </div>
+  ); 
+};
+
+export default Home;
